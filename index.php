6d0fe4107d674d0a8e368ffb974be0fed461d463
TimonAME <timon-a@outlook.at>
2023-10-25 10:08:33 +0200

    Git beschreibung

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
+/*
+ * GitHub:
+ * Repository forked then cloned
+ * Branch created, Timon and Momo
+ * Switch branches with git checkout
+ * Changes made, git add, git commit, git push
+ * Merge into main branch with git checkout main, git merge Timon/Momo
+ * Then git checkout Timon/Momo, git merge main
+ */
 #Test
